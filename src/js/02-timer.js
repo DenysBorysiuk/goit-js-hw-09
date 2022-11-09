096083de231412690c7f59287092c56b9d761ea6
DenysBorysiuk <denisborisiuk@gmail.com>
2022-11-09 16:40:42 +0200

    finish task-02

---
+++ src/js/02-timer.js
@@ -0,0 +1,91 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+let selectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      // window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+    } else {
+      selectedDate = selectedDates[0];
+      refs.startBtn.disabled = false;
+    }
+    // console.log(selectedDates[0]);
+  },
+};
+
+const pickr = flatpickr('#datetime-picker', options);
+
+const timer = {
+  start() {
+    const endTime = selectedDate.getTime();
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = endTime - currentTime;
+      const timeComponents = convertMs(deltaTime);
+      udateClockFace(timeComponents);
+      console.log(timeComponents);
+    }, 1000);
+  },
+};
+
+refs.startBtn.addEventListener('click', onClickStart);
+
+function onClickStart() {
+  timer.start();
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function udateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+// e.g. Only message
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
