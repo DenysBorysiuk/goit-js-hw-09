a77841f08a353ab0d68b3727f16040c576e06c79
DenysBorysiuk <denisborisiuk@gmail.com>
2022-11-09 07:41:08 +0200

    to process task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+refs.btnStart.addEventListener('click', onClickStart);
+refs.btnStop.addEventListener('click', onClickStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStart(evt) {
+  console.log('start');
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onClickStop(evt) {
+  console.log('stop');
+}
